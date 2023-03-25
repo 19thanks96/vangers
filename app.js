d78dbd25a3dfcc6c02c47ab85627ac729abedcbc
hellgamer <19thanks96@gmail.com>
2023-03-25 19:00:56 +0200

    end lesson

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-document.body.innerHTML += "жиес зогружен!";
+let mars = { 
+        x: 100, 
+        y: 100, 
+
+    };
+let sun = { 
+        x: 0, 
+        y: 500,
+    };
+
+
+let particles = [ 
+      mars, sun
+    
+];
+
+particles.forEach(printParticle)
+
+function printParticle(p, index) {
+    const id = 'particle' + index
+    document.body.innerHTML += `<img id="${id}" src="particle.jpeg">`;
+    p.element = document.querySelector(`#${id}`)
+    console.log(p)
+    p.element.style.position = 'absolute';
+    p.element.style.width = 100 + 'px'
+    p.element.style.left = p.x + 'px';
+    p.element.style.top = p.y + 'px';
+}
